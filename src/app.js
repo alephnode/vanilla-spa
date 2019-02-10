3ed63ea7a75205a14865747d6dc6609911352909
srmward <srmward@gmail.com>
2019-02-09 22:29:08 -0800

    Flesh Out App.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { html } from 'lit-html'
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
 import Base from './base'
 import registerComponent from './common/register-component'
 import './components/v-headline'
@@ -7,20 +8,42 @@ import './pages/page-one'
 import './pages/page-two'
 
 class VApp extends Base {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  onMount() {
+    this.toggleActivePage()
+  }
+
+  toggleActivePage() {
+    let newPage = this.activePage === 'v-page-one' ? 'v-page-two' : 'v-page-one'
+    this.activePage = newPage
+    const pageTag = `<${this.activePage}></${this.activePage}>`
+    this._htmlToRender = html`
+      ${unsafeHTML(pageTag)}
+    `
+    this._render()
+  }
+
   handleClick(e) {
     e.preventDefault()
-    console.log('herd dat')
+    this.toggleActivePage()
+    this._render()
   }
 
   _html() {
-    return html`
-      <div>
-        <v-headline text="woot"></v-headline>
-        <v-graph text="there it is"></v-graph>
-        <v-page-one></v-page-one>
-        <v-page-two></v-page-tw0>
-      </div>
-    `
+    return this._htmlToRender
+      ? html`
+          <div>
+            <v-headline text="woot"></v-headline>
+            <v-graph text="there it is"></v-graph>
+            ${this._htmlToRender}
+            <button @click=${this.handleClick}>turn the page</button>
+          </div>
+        `
+      : ``
   }
 }
 
