730647fd2b5f5eab931b1f5064d31903f92ceee7
srmward <srmward@gmail.com>
2019-02-09 20:48:56 -0800

    Push Progress

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
+import Base from './base.js'
 import './components/v-headline/index.js'
 import './components/v-graph/index.js'
+
+class VApp extends Base {
+  html() {
+    const tpl = `
+          <div>
+            <v-headline text="Welcome to the new vanilla site!"></v-headline>
+            <v-graph text="Even more content coming soon ..."></v-graph>
+          </div>
+        `
+    return tpl
+  }
+}
+
+;(() => {
+  const register = () => customElements.define('v-app', VApp)
+  window.WebComponents ? window.WebComponents.waitFor(register) : register()
+})()
